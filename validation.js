f543958ab88ed5759243c3894f1b929ba95bb8e6
Sidi-Ali <sideee05@yahoo.com>
2023-01-19 17:37:24 +0000

    Modify index and validation

--- validation.js
+++ validation.js
@@ -1,13 +1,16 @@
 var email = document.getElementById("mail");
 
 email.addEventListener("invalid", function(event){
-  if (event.target.validity.typeMismatch){
-    // console.log("this is invalid.");
+  if (event.target.validity.typeMismatch || email.value.length === 0){
     document.querySelector("form img").classList.remove("showError");
     document.getElementById("err1").classList.remove("showError");
     document.querySelector("form").classList.add("email-div-error");
-    //event.target.setCustomValidity("xxxxxxxxxx");
     event.target.setCustomValidity(" ");
 
+    setTimeout(function () {
+      document.querySelector("form img").classList.add("showError");
+      document.getElementById("err1").classList.add("showError");
+      document.querySelector("form").classList.remove("email-div-error");
+    }, 5000);
   }
 })
